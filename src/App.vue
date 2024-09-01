6eac17d1d0cdfb669966ead8cd25fa27e64e65c2
jaaviieerx <javiertjh@gmail.com>
2024-09-02 01:35:39 +0800

    [1_UPLOAD] complete

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  <UploadFile video="" />
+</template>
+
+<script>
+import UploadFile from './components/UploadFile.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UploadFile
+  },
+  mounted() {
+    const openCvScript = document.createElement('script')
+    openCvScript.src = "./scripts/opencv.js"
+    openCvScript.type = "text/javascript"
+    document.head.appendChild(openCvScript)
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Poppins", sans-serif;
+}
+
+#app {
+  position: relative;
+  min-height: 100vh;
+  min-width: 100%;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #5256ad;
+}
+</style>
---
+++ src/components/UploadFile.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="drag-area" ref="dragArea" @dragleave="onDragleave()" @dragover.prevent="onDragover()"
+    @drop.prevent="onDrop($event)">
+    <div class="icon"><i class="fas fa-cloud-upload-alt"></i></div>
+    <header id="drag-area-header" ref="dragText">drag & drop to upload video</header>
+    <span id="drag-area-span">- or -</span>
+    <button id="browse-btn" @click="onClick()">browse file</button>
+    <input id="file-input" type="file" hidden ref="fileInput" @change="onChange($event)">
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+let file;
+
+const dragArea = ref(null)
+const fileInput = ref(null)
+const dragText = ref(null)
+
+function validateFile() {
+  console.log(file)
+}
+
+export default {
+  name: 'UploadFile',
+  props: {
+    video: undefined
+  },
+  setup() {
+    return { dragArea, fileInput, dragText };
+  },
+  methods: {
+    onClick() {
+      fileInput.value.click()
+    },
+    onChange() {
+      file = this.files[0];
+      dragArea.value.classList.add('active');
+      validateFile();
+    },
+    onDragover() {
+      dragArea.value.classList.add('active');
+      dragText.value.textContent = 'Release to Upload File';
+    },
+    onDragleave() {
+      dragArea.value.classList.remove('active');
+      dragText.value.textContent = 'drag & drop to upload video';
+    },
+    onDrop(e) {
+      file = e.dataTransfer.files[0];
+      validateFile();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#drag-area {
+  border: 2px dashed #fff;
+  height: 80vh;
+  width: 80%;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  z-index: 3;
+}
+
+#drag-area.active {
+  border: 2px solid #fff;
+}
+
+#drag-area .icon {
+  font-size: 100px;
+  color: #fff;
+}
+
+#drag-area-header {
+  font-size: 30px;
+  font-weight: 500;
+  color: #fff;
+}
+
+#drag-area-span {
+  font-size: 25px;
+  font-weight: 500;
+  color: #fff;
+  margin: 10px 0 15px 0;
+}
+
+#browse-btn {
+  padding: 10px 25px;
+  font-size: 20px;
+  font-weight: 500;
+  border: none;
+  outline: none;
+  background: #fff;
+  color: #5256ad;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
